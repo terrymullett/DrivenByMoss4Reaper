838ea5bfee526c57e9775f3e49f3ad4bd9fdd7a6
git-moss <juergen.mossgraber@googlemail.com>
2020-07-16 19:26:11 +0200

    Update to JDK 14. JNA 5.6.

--- runtime/buildReducedJVM.cmd
+++
@@ -1,4 +0,0 @@
-set DESTINATION_FOLDER=windows\java-runtime
-
-rmdir /Q /S "%DESTINATION_FOLDER%"
-"%JAVA_HOME13%/bin/jlink" --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.desktop,java.xml,java.naming,jdk.accessibility --output %DESTINATION_FOLDER%
