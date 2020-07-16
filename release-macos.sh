b2e2c13338d416277fea1695984fe75f2b646efc
mos <mos@moss-mac-mini.fritz.box>
2020-07-16 21:57:57 +0200

    Update to JDK 14.

--- release-macos.sh
+++ release-macos.sh
@@ -1,3 +1,3 @@
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-13.jdk/Contents/Home/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-14.jdk/Contents/Home/
 
 ./gradlew clean generateLicenseReport installDist
