ecfdcbb3c10d6a653152ea653603b5d5e739a9ff
mos <mos@UbuntuStudioLenovo>
2020-10-24 11:10:46 +0200

    Updated Linux JDK to 15.0.1.

--- runtime/buildReducedJVM-linux.sh
+++ runtime/buildReducedJVM-linux.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/home/mos/java/jdk-15+36
+export JAVA_HOME=/home/mos/java/jdk-15.0.1+9/
 export DESTINATION_FOLDER=linux/java-runtime
 rm -drf $DESTINATION_FOLDER
 $JAVA_HOME/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.desktop,java.instrument,java.management,java.naming,java.sql,java.logging,jdk.accessibility --output $DESTINATION_FOLDER
