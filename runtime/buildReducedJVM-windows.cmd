a2a10316347cbdf69e75e243adcc7b463e282154
git-moss <juergen.mossgraber@googlemail.com>
2020-11-01 14:47:53 +0100

    Reduced JDK compression to 1 on Windows 10 to prevent crash.

--- runtime/buildReducedJVM-windows.cmd
+++ runtime/buildReducedJVM-windows.cmd
@@ -1,5 +1,5 @@
 set DESTINATION_FOLDER=windows\java-runtime
 
 rmdir /Q /S "%DESTINATION_FOLDER%"
-"%JAVA_HOME15%/bin/jlink" --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.desktop,java.instrument,java.management,java.naming,java.sql,java.logging,jdk.accessibility --output %DESTINATION_FOLDER%
+"%JAVA_HOME15%/bin/jlink" --no-header-files --no-man-pages --compress=1 --strip-debug --add-modules java.desktop,java.instrument,java.management,java.naming,java.sql,java.logging,jdk.accessibility --output %DESTINATION_FOLDER%
 
