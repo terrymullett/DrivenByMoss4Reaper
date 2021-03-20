20432a91cab2268d61cac49c236c6303c2a4bf1a
Jürgen Moßgraber <mos@moss-mac-mini.fritz.box>
2021-03-20 20:27:53 +0100

    Updated to JDK 16.

--- runtime/buildReducedJVM-macos.sh
+++ runtime/buildReducedJVM-macos.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-15.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home
 export DESTINATION_FOLDER=macos/java-runtime
 rm -drf $DESTINATION_FOLDER
 $JAVA_HOME/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.desktop,java.instrument,java.management,java.naming,java.sql,java.logging,jdk.accessibility --output $DESTINATION_FOLDER
