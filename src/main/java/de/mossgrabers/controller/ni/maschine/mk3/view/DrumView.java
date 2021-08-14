e1fb6c52158a19e121449a2b4774fb7bc6bbe99c
git-moss <juergen.mossgraber@googlemail.com>
2021-08-14 11:58:26 +0200

    Integrated DBM 13.6.

--- src/main/java/de/mossgrabers/controller/ni/maschine/mk3/view/DrumView.java
+++ src/main/java/de/mossgrabers/controller/ni/maschine/mk3/view/DrumView.java
@@ -7,11 +7,11 @@ package de.mossgrabers.controller.ni.maschine.mk3.view;
 import de.mossgrabers.controller.ni.maschine.core.MaschineColorManager;
 import de.mossgrabers.controller.ni.maschine.mk3.MaschineConfiguration;
 import de.mossgrabers.controller.ni.maschine.mk3.controller.MaschineControlSurface;
-import de.mossgrabers.controller.ni.maschine.mk3.mode.EditNoteMode;
 import de.mossgrabers.framework.controller.ButtonID;
 import de.mossgrabers.framework.controller.grid.IPadGrid;
 import de.mossgrabers.framework.daw.IModel;
 import de.mossgrabers.framework.daw.INoteClip;
+import de.mossgrabers.framework.daw.IStepInfo;
 import de.mossgrabers.framework.daw.StepState;
 import de.mossgrabers.framework.daw.data.IDrumDevice;
 import de.mossgrabers.framework.featuregroup.ModeManager;
@@ -134,7 +134,7 @@ public class DrumView extends AbstractDrumView<MaschineControlSurface, MaschineC
             final INoteClip clip = this.getClip ();
             final boolean isActive = this.isActive ();
             final IDrumDevice primary = this.model.getDrumDevice ();
-            this.drawSequencerSteps (clip, isActive, this.scales.getDrumOffset () + this.selectedPad, this.getDrumPadColor (primary, this.selectedPad), y -> 3 - y);
+            this.drawSequencerSteps (clip, isActive, this.scales.getDrumOffset () + this.selectedPad, this.getPadColor (primary, this.selectedPad), y -> 3 - y);
             return;
         }
 
@@ -236,12 +236,20 @@ public class DrumView extends AbstractDrumView<MaschineControlSurface, MaschineC
     @Override
     protected boolean handleSequencerAreaButtonCombinations (final INoteClip clip, final int channel, final int step, final int note, final int velocity)
     {
+        if (this.isButtonCombination (ButtonID.MUTE))
+        {
+            final IStepInfo stepInfo = clip.getStep (channel, step, note);
+            final StepState isSet = stepInfo.getState ();
+            if (isSet == StepState.START)
+                this.getClip ().updateMuteState (channel, step, note, !stepInfo.isMuted ());
+            return true;
+        }
+
         final ModeManager modeManager = this.surface.getModeManager ();
         if (modeManager.isActive (Modes.NOTE))
         {
-            final StepState isSet = clip.getStep (channel, step, note).getState ();
             this.model.getHost ().showNotification ("Note " + Scales.formatNoteAndOctave (note, -3) + " - Step " + Integer.toString (step + 1));
-            ((EditNoteMode) modeManager.get (Modes.NOTE)).setValues (isSet == StepState.START ? clip : null, channel, step, note);
+            this.editNote (clip, channel, step, note, true);
             return true;
         }
 
