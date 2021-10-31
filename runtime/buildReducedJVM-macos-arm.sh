db7c4e33600dd685c40b84af99cf2304f847d598
mos <mos@mac-mini-von-jurgen.fritz.box>
2021-10-31 14:13:58 +0100

    Update Macos ARM JDK to 17.

--- runtime/buildReducedJVM-macos-arm.sh
+++ runtime/buildReducedJVM-macos-arm.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-16.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home
 export DESTINATION_FOLDER=macos-arm/java-runtime
 rm -drf $DESTINATION_FOLDER
 $JAVA_HOME/bin/jlink --no-header-files --no-man-pages --compress=1 --strip-debug --add-modules java.desktop,java.instrument,java.management,java.naming,java.sql,java.logging,jdk.accessibility --output $DESTINATION_FOLDER
