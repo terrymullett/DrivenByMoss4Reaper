cfe791febc0e0c66994c999abc0f6a52ccc5dfa0
git-moss <juergen.mossgraber@googlemail.com>
2021-10-31 15:09:49 +0100

    Updated Windows JDK to 17.

--- runtime/buildReducedJVM-windows.cmd
+++ runtime/buildReducedJVM-windows.cmd
@@ -1,4 +1,4 @@
 set DESTINATION_FOLDER=windows\java-runtime
 
 rmdir /Q /S "%DESTINATION_FOLDER%"
-"%JAVA_HOME16%/bin/jlink" --no-header-files --no-man-pages --compress=1 --strip-debug --add-modules java.desktop,java.instrument,java.management,java.naming,java.sql,java.logging,jdk.accessibility --output %DESTINATION_FOLDER%
+"%JAVA_HOME17%/bin/jlink" --no-header-files --no-man-pages --compress=1 --strip-debug --add-modules java.desktop,java.instrument,java.management,java.naming,java.sql,java.logging,jdk.accessibility --output %DESTINATION_FOLDER%
