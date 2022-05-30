67d86c406f7da80de1592f1d0b65b3f41df1aa58
mos <mos@UbuntuStudioLenovo>
2022-05-30 18:05:57 +0200

    Updated Linux Jdk to 17.0.3.

--- runtime/buildReducedJVM-linux.sh
+++ runtime/buildReducedJVM-linux.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=export JAVA_HOME=/home/mos/java/jdk-17.0.1+12/
+export JAVA_HOME=export JAVA_HOME=/home/mos/java/jdk-17.0.3+7/
 export DESTINATION_FOLDER=linux/java-runtime
 rm -drf $DESTINATION_FOLDER
 $JAVA_HOME/bin/jlink --no-header-files --no-man-pages --compress=1 --strip-debug --add-modules java.desktop,java.instrument,java.management,java.naming,java.sql,java.logging,jdk.accessibility --output $DESTINATION_FOLDER
