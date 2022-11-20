91993fa810ffd068be7bcc4709e01a563ef19289
mos <mos@mac-mini-von-jurgen.fritz.box>
2022-11-20 17:41:35 +0100

    Updated JDKs to 17.0.5+8

--- runtime/buildReducedJVM-macos.sh
+++ runtime/buildReducedJVM-macos.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/Users/mos/Documents/Development/Jdk-x64/jdk-17.0.4.1+1/Contents/Home
+export JAVA_HOME=/Users/mos/Documents/Development/Jdk-x64/jdk-17.0.5+8/Contents/Home
 export DESTINATION_FOLDER=macos/java-runtime
 rm -drf $DESTINATION_FOLDER
 $JAVA_HOME/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.desktop,java.instrument,java.management,java.naming,java.sql,java.logging,jdk.accessibility --output $DESTINATION_FOLDER
