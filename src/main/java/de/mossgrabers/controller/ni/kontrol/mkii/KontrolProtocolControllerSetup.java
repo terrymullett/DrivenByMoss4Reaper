19dffce598eab23b509800019219e75a7b261480
Jürgen Moßgraber <juergen.mossgraber@googlemail.com>
2023-05-01 19:55:38 +0200

    Switched to from user to project parameters.

--- src/main/java/de/mossgrabers/controller/ni/kontrol/mkii/KontrolProtocolControllerSetup.java
+++ src/main/java/de/mossgrabers/controller/ni/kontrol/mkii/KontrolProtocolControllerSetup.java
@@ -77,8 +77,8 @@ public class KontrolProtocolControllerSetup extends AbstractControllerSetup<Kont
 {
     private final int          version;
     private String             kompleteInstance   = "";
-    private final Object             navigateLock       = new Object ();
-    private final LatestTaskExecutor slotScrollExecutor = new LatestTaskExecutor ();
+    private Object             navigateLock       = new Object ();
+    private LatestTaskExecutor slotScrollExecutor = new LatestTaskExecutor ();
     private long               lastEdit;
 
 
@@ -538,7 +538,7 @@ public class KontrolProtocolControllerSetup extends AbstractControllerSetup<Kont
             this.lastEdit = System.currentTimeMillis ();
         }
 
-        this.slotScrollExecutor.execute ( () -> this.selectSlot (slotIndex));
+        this.slotScrollExecutor.execute ( () -> selectSlot (slotIndex));
     }
 
 
@@ -562,7 +562,7 @@ public class KontrolProtocolControllerSetup extends AbstractControllerSetup<Kont
             if (System.currentTimeMillis () - this.lastEdit > 200)
                 this.model.getCursorTrack ().getSlotBank ().getItem (slotIndex).select ();
             else
-                this.slotScrollExecutor.execute ( () -> this.selectSlot (slotIndex));
+                this.slotScrollExecutor.execute ( () -> selectSlot (slotIndex));
         }
     }
 
