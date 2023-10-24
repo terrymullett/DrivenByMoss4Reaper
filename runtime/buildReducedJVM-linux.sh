991d42658665eb4f1447add4e655c0f672bb5707
mos <mos@UbuntuStudioLenovo>
2023-10-24 23:54:18 +0200

    Updated to Linux JDK 17.0.9+9.

--- runtime/buildReducedJVM-linux.sh
+++ runtime/buildReducedJVM-linux.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=export JAVA_HOME=/home/mos/java/jdk-17.0.7+7/
+export JAVA_HOME=export JAVA_HOME=/home/mos/java/jdk-17.0.9+9/
 export DESTINATION_FOLDER=linux/java-runtime
 rm -drf $DESTINATION_FOLDER
 $JAVA_HOME/bin/jlink --no-header-files --no-man-pages --compress=1 --strip-debug --add-modules java.desktop,java.instrument,java.management,java.naming,java.sql,java.logging,jdk.accessibility --output $DESTINATION_FOLDER
