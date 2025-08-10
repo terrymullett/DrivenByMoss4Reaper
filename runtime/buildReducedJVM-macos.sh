0ae96693829d27e6ea871a28daaffe1240241e50
mos <mos@mac-mini-von-jurgen.fritz.box>
2025-08-10 23:40:45 +0200

    Updated to latest 24 JDKs.

--- runtime/buildReducedJVM-macos.sh
+++ runtime/buildReducedJVM-macos.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/Users/mos/Documents/Development/Jdk-x64/jdk-24.0.1+9/Contents/Home
+export JAVA_HOME=/Users/mos/Documents/Development/Jdk-x64/jdk-24.0.2+12/Contents/Home
 export DESTINATION_FOLDER=macos/java-runtime
 rm -drf $DESTINATION_FOLDER
 $JAVA_HOME/bin/jlink --no-header-files --no-man-pages --strip-debug --add-modules java.desktop,java.instrument,java.management,java.naming,java.sql,java.logging,jdk.accessibility --output $DESTINATION_FOLDER
