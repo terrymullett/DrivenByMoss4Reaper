2e4536266a4b71935f259145a89bcba1dc2f6561
mos <mos@UbuntuStudioLenovo>
2025-10-12 17:03:18 +0200

    Updated Linux JDK to 25.

--- runtime/buildReducedJVM-linux.sh
+++ runtime/buildReducedJVM-linux.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/home/mos/java/jdk-25.0.2+36
+export JAVA_HOME=/home/mos/java/jdk-25+36
 export DESTINATION_FOLDER=linux/java-runtime
 rm -drf $DESTINATION_FOLDER
 $JAVA_HOME/bin/jlink --no-header-files --no-man-pages --strip-debug --add-modules java.desktop,java.instrument,java.management,java.naming,java.sql,java.logging,jdk.accessibility --output $DESTINATION_FOLDER
